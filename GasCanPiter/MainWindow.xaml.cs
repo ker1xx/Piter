c4a92ad047987b453aa92ff9e15cbd81f7d7a746
ker1xx <klinkamama@gmail.com>
2023-07-07 23:02:39 +0300

    gas

---
+++ GasCanPiter/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GasCanPiter
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _codeWord;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _codeWord = _generateCodeWord();
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            int getID = 0;
+            TCPServer.OnNewMessage += (msg) =>
+            {
+                getID = Convert.ToInt32(msg);
+            };
+            TCPServer.Start(AZS_ID.Text);
+        }
+        private string _generateCodeWord()
+        {
+            char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+            Random rand = new Random();
+            string word = "";
+            for (int j = 1; j <= 12; j++)
+            {
+                int letter_num = rand.Next(0, letters.Length - 1);
+                word += letters[letter_num];
+            }
+            return word;
+        }
+        private async void Init()
+        {
+            List<StationModel> List = new List<StationModel>();
+            HttpClient client = new HttpClient();
+            var response = client.GetAsync(apiurl + "Stations/" + ID_Input.Text).Result;
+            response.EnsureSuccessStatusCode();
+            List = response.Content.ReadAsStringAsync<List<StationModel>>();
+        }
+    }
+}
---
+++ GasCanPiter/TCPServer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GasCanPiter
+{
+    internal class TCPServer
+    {
+        private static Socket _socket;
+        private static List<Socket> _clients = new List<Socket>();
+
+        public delegate void NewMessageCallback(string message);
+        public delegate void NewClientCallback(Socket client);
+
+        public static event NewMessageCallback OnNewMessage;
+        public static event NewClientCallback OnNewClient;
+
+        public static void Start(string id)
+        {
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, Convert.ToInt32("102" + id));
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _socket.Bind(ipEndPoint);
+            _socket.Listen(10);
+
+            ListenToClients();
+        }
+
+        private static async Task ListenToClients()
+        {
+            while (true)
+            {
+                var newClient = await _socket.AcceptAsync();
+                _clients.Add(newClient);
+
+                OnNewClient(newClient);
+
+                await ReceiveMessages(newClient);
+            }
+        }
+
+        private static async Task ReceiveMessages(Socket clientReceived)
+        {
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+
+                await clientReceived?.ReceiveAsync(new ArraySegment<byte>(buffer), SocketFlags.None);
+
+                string message = Encoding.UTF8.GetString(buffer);
+                OnNewMessage(message);
+            }
+        }
+    }
+}
