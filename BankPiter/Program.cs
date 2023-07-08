cf6922f4f2e1164bb4110a57745f1938e1f77a88
ker1xx <klinkamama@gmail.com>
2023-07-08 12:57:39 +0300

    bang

---
+++ BankPiter/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Web;
+using System.Runtime.Remoting.Contexts;
+using static System.Net.WebRequestMethods;
+
+namespace BankPiter
+{
+    internal class Program
+    {
+        static string AnswerCode;
+        static bool _isAllFine;
+        static void Main(string[] args)
+        {
+            transcation(args);
+            
+        }
+        static async void transcation(string[] args)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                TransactionModel newTransact = new TransactionModel(Convert.ToInt32(args[0]), args[1],
+                    Convert.ToInt32(args[2]), Convert.ToSingle(args[3]),
+                    args[4], Convert.ToInt32(args[5]), args[6]);
+                var url = "https://localhost:7011/api/BankTransactions";
+                webClient.Headers[HttpRequestHeader.Accept] = "text/plain";
+                webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
+                string data = JsonConvert.SerializeObject(newTransact);
+                var response = webClient.UploadString(url, data);
+            }
+            Random rand = new Random();
+            int isAllowed = rand.Next(1, 2);
+            if (isAllowed == 1)
+            {
+                AnswerCode = DateTime.Now.ToString("G");
+            }
+            else if (isAllowed == 2)
+            {
+                AnswerCode = "00000000-000000";
+            }
+            if(isTimeFine())
+            {
+                _isAllFine = true;
+            }
+
+        }
+        static bool isTimeFine()
+        {
+            Random rand = new Random();
+            int time = rand.Next(1, 20);
+            for (int i = 0; i < time; i++)
+            {
+                Thread.Sleep(1000);
+            }
+            if (time <= 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ BankPiter/TransactionModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankPiter
+{
+    internal class TransactionModel
+    {
+        public TransactionModel(int CardNumber, string NameSurname, int AuthorizationCode,float Price, string OrgranizationName, int AzsId, string SessionKey)
+        {
+            this.CardNubmer = CardNubmer;
+            this.NameSurname = NameSurname;
+            this.AuthorizationCode = AuthorizationCode;
+            this.Price = Price;
+            this.OrgranizationName = OrgranizationName;
+            this.AzsId = AzsId;
+            this.SessionKey = SessionKey;
+        }
+        public int? Id { get; set; }
+
+        public int CardNubmer { get; set; }
+
+        public string NameSurname { get; set; } = null;
+
+        public int AuthorizationCode { get; set; }
+
+        public double Price { get; set; }
+
+        public string OrgranizationName { get; set; } = null;
+
+        public int AzsId { get; set; }
+
+        public string SessionKey { get; set; } = null;
+    }
+}
